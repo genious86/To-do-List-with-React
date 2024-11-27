7bf107f9ecb7a388577ef0a591ce3e1d39363f45
genious86 <pauleugenius86@gmail.com>
2024-11-27 10:34:34 +0300

    writing  the needed functions

--- ToDo_List/src/ToDoList.jsx
+++ ToDo_List/src/ToDoList.jsx
@@ -4,6 +4,24 @@ import React,{useState} from "react"
 function ToDoList(){
 
     const [tasks, SetTask] = useState([])
+    const [newtasks, SetNewTask] = useState("")
+
+    function addtask (){
+
+    }
+
+    function deletetask (index){
+
+    }
+
+    function movetaskup (index){
+
+    }
+
+    function movetaskdown (index){
+
+    }
+    
     return(<></>)
 }
 
